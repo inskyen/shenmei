37bd1734301a67ee9e8f52de54a09e7e032479bb
skyyint <skyyint@macdeMacBook-Pro.local>
2026-07-10 00:40:44 +0800

    fix: add global meta referrer tag to bypass Bilibili image hotlinking protection

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="referrer" content="no-referrer" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
