bb3e150536fc2fdced04756b00da4c5535509288
skyyint <skyyint@macdeMacBook-Pro.local>
2026-07-13 10:09:01 +0800

    feat: add social sharing icon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,14 @@ export default function Document() {
     <Html lang="zh-Hant-TW">
       <Head>
         <link rel="icon" href="/brand/butterfly.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/brand/butterfly.png" />
+        <meta property="og:site_name" content="審美者" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://shenmei.org/brand/butterfly.png" />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="1024" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:image" content="https://shenmei.org/brand/butterfly.png" />
       </Head>
       <body className="antialiased">
         <Main />
